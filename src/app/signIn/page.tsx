f9283f2ddc2a27acbf286b41d00fe917ddb3b11e
ukhan1219 <113568605+ukhan1219@users.noreply.github.com>
2024-10-05 01:47:28 -0400

    button testing

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,7 +1,6 @@
 import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 import { GoogleSignInButton, DiscordSignInButton} from  "~/app/_components/authButtons";
-import { signIn, signOut, useSession } from "next-auth/react"
 
 export default async function SignInPage() {
 
@@ -10,7 +9,7 @@ export default async function SignInPage() {
         <HydrateClient>
             <div className="bg-site-background min-h-screen text-white w-full flex flex-col items-center justify-center py-2">
                 <div className="bg-site-foreground flex flex-col items-center w-1/3 mt-10 p-10 shadow-md rounded-lg">
-                    <h1 className="mt-10 mb-4 text-4xl font-bold">Sign In</h1>
+                    <h1 className="mt-5 mb-4 text-4xl font-bold">Sign In</h1>
                     <GoogleSignInButton/>
                     <DiscordSignInButton/>
 
