3bf0edf1d5615c8a794a4c8ced6c66a4d541fda4
ukhan1219 <113568605+ukhan1219@users.noreply.github.com>
2024-10-05 17:59:39 -0400

    coinbase

--- src/app/_components/authButtons.tsx
+++ src/app/_components/authButtons.tsx
@@ -13,7 +13,7 @@ export function GoogleSignInButton() {
     };
 
     return (
-        <button onClick={handleClick} className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200">
+        <button onClick={handleClick} className="fira-sans-regular w-full flex items-center justify-center h-14 px-6 mt-4 text-l font-extrabold transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200">
             <Image src={googleLogo} alt="Google Logo" width={20} height={20}/>
             <span className="ml-4">Sign in with Google</span>
         </button>
@@ -25,7 +25,7 @@ export function DiscordSignInButton() {
     };
 
     return (
-        <button onClick={handleClick} className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200">
+        <button onClick={handleClick} className="fira-sans-regular w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-l transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200">
             <Image src={discordLogo} alt="Discord Logo" width={20} height={20}/>
             <span className="ml-4">Sign in with Discord</span>
         </button>
@@ -37,9 +37,9 @@ export function CoinbaseSignInButton() {
     };
 
     return (
-        <button onClick={handleClick} className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200">
-            <Image src={coinbaseLogo} alt="Google Logo" width={20} height={20}/>
-            <span className="ml-4">Sign in with Google</span>
+        <button onClick={handleClick} className="fira-sans-regular w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-l transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200">
+            <Image src={coinbaseLogo} alt="Coinbase Logo" width={20} height={20}/>
+            <span className="ml-4">Sign in with Coinbase</span>
         </button>
     );
 }
