6e54765c9689855fe011d17fc977e0ccbe27694e
ukhan1219 <113568605+ukhan1219@users.noreply.github.com>
2024-10-05 01:16:12 -0400

    added signin

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -9,7 +9,7 @@ export default async function SignInPage() {
     return (
         <HydrateClient>
             <div className="bg-site-background min-h-screen text-white w-full flex flex-col items-center justify-center py-2">
-                <div className="bg-site-foreground flex flex-col items-center w-1/3 mt-10 p-10 shadow-md">
+                <div className="bg-site-foreground flex flex-col items-center w-1/3 mt-10 p-10 shadow-md rounded-lg">
                     <h1 className="mt-10 mb-4 text-4xl font-bold">Sign In</h1>
                     <GoogleSignInButton/>
                     <DiscordSignInButton/>
