79ab3a96ebf41320dae80a73a6008711db113925
ukhan1219 <113568605+ukhan1219@users.noreply.github.com>
2024-10-05 01:10:39 -0400

    singin

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -11,8 +11,8 @@ export default async function SignInPage() {
             <div className="bg-site-background min-h-screen text-white w-full flex flex-col items-center justify-center py-2">
                 <div className="bg-site-foreground flex flex-col items-center w-1/3 mt-10 p-10 shadow-md">
                     <h1 className="mt-10 mb-4 text-4xl font-bold">Sign In</h1>
-                    {/* <GoogleSignInButton/> */}
-                    {/* <DiscordSignInButton/> */}
+                    <GoogleSignInButton/>
+                    <DiscordSignInButton/>
 
                 </div>
 
