7bb34b123694e0d7971854731d0aefb0c23ddbe3
ukhan1219 <113568605+ukhan1219@users.noreply.github.com>
2024-10-05 00:36:23 -0400

    sign in

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,14 +1,16 @@
 import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
+import { GoogleSignInButton } from  "~/app/_components/GoogleSignInButton";
+import { DiscordSignInButton } from  "~/app/_components/DiscordSignInButton";
+import { signIn, signOut, useSession } from "next-auth/react"
 
-export default async function Home() {
+export default async function SignInPage() {
 
 
-  return (
-    <HydrateClient>
-      <div>
-
-      </div>
-    </HydrateClient>
-  );
+    return (
+        <HydrateClient>
+            <div className="bg-site-background min-h-screen">
+            </div>
+        </HydrateClient>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      colors: {
+        'site-background': '#292464',
+      },
     },
   },
   plugins: [],
